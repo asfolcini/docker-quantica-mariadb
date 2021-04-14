ce5c0df6ff2fc484dc69bab1230dba5f9b116f89
Alberto Sfolcini <a.sfolcini@gmail.com>
2021-04-14 15:17:27 +0200

    2021-04-14-8

--- 0-init.sql
+++ 0-init.sql
@@ -1,3 +1,8 @@
-CREATE DATABASE IF NOT EXISTS 'db-quantica-core';
-GRANT ALL PRIVILEGES ON 'db-quantica-core'.* TO 'quantica_usr'@'%' IDENTIFIED BY 'quantica_psw';
+CREATE DATABASE IF NOT EXISTS `db-quantica-core` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+CREATE USER IF NOT EXISTS 'quantica_usr'@'%';
+ALTER USER 'quantica_usr'@'%' IDENTIFIED BY 'quantica_psw' ;
+
+GRANT ALL ON `db-quantica-core`.* TO 'quantica_usr'@'%';
+
 FLUSH PRIVILEGES;
